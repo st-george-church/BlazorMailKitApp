cf992a133f0db6060e7e75d3cde5c4d46cccbe8e
Medhat Elmasry <medhat@elmasry.ca>
2025-04-22 16:31:06 -0700

    try again

--- Services/GmailSmtpService.cs
+++ Services/GmailSmtpService.cs
@@ -7,27 +7,38 @@ namespace BlazorMailKitApp.Services;
 public class GmailSmtpService(IConfiguration _config)
 {
 
-    public async Task SendEmailAsync(string receptor, string subject, string body)
+    public async Task SendEmailAsync(string receptor, string subject, string body, bool isHtml = false)
     {
-        var email = _config.GetValue<string>("EMAIL_CONFIGURATION:EMAIL");
-        var password = _config.GetValue<string>("EMAIL_CONFIGURATION:PASSWORD");
-        var host = _config.GetValue<string>("EMAIL_CONFIGURATION:HOST");
-        var port = _config.GetValue<int>("EMAIL_CONFIGURATION:PORT");
+        var fromEmail = _config.GetValue<string>("GmailSmtpConfig:FromEmail");
+        var fromName = _config.GetValue<string>("GmailSmtpConfig:FromName");
+        var password = _config.GetValue<string>("GmailSmtpConfig:Password");
+        var host = _config.GetValue<string>("GmailSmtpConfig:Host");
+        var port = _config.GetValue<int>("GmailSmtpConfig:Port");
 
-        Console.WriteLine($"Email: {email}");
-        Console.WriteLine($"Password: {password}");
-        Console.WriteLine($"Host: {host}");
-        Console.WriteLine($"Port: {port}");
-        Console.WriteLine($"Receptor: {receptor}");
-        Console.WriteLine($"Subject: {subject}");
-        Console.WriteLine($"Body: {body}");
+        // Console.WriteLine($"Email: {fromEmail}");
+        // Console.WriteLine($"Password: {password}");
+        // Console.WriteLine($"Host: {host}");
+        // Console.WriteLine($"Port: {port}");
+        // Console.WriteLine($"Receptor: {receptor}");
+        // Console.WriteLine($"Subject: {subject}");
+        // Console.WriteLine($"Body: {body}");
 
         var smtpClient = new SmtpClient(host, port);
         smtpClient.EnableSsl = true;
         smtpClient.UseDefaultCredentials = false;
 
-        smtpClient.Credentials = new NetworkCredential(email, password);
-        var message = new MailMessage(email!, receptor, subject, body);
+        smtpClient.Credentials = new NetworkCredential(fromEmail, password);
+        // var message = new MailMessage(fromEmail!, receptor, subject, body);
+        
+        var message = new MailMessage
+        {
+            From = new MailAddress(fromEmail!, fromName),
+            Subject = subject,
+            Body = body,
+            To = { new MailAddress(receptor) },
+            IsBodyHtml = isHtml
+        };
+        
         await smtpClient.SendMailAsync(message);
     }
 
